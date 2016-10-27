6a71943bc68297378a0d1ecb92bd5f7386b0ca0c
Abdullah Alger <abdullah.alger@ibm.com>
2016-10-27 11:31:03 -0700

    added python

---
+++ mysql-connect.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+config = {  
+    'user': 'admin',
+    'password': 'mypass',
+    'host': 'aws-us-east-1-portal.23.dblayer.com',
+    'port': 15942,
+    'database': 'compose',
+
+    # Create SSL connection with the self-signed certificate
+    'ssl_verify_cert': True,
+    'ssl_ca': 'cert.pem'
+}
+
+connect = mysql.connector.connect(**config)  
+cur = connect.cursor()  
+cur.execute("SHOW DATABASES")
+
+for row in cur:  
+    print(row[0])
+
+connect.close()  
