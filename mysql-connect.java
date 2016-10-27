c00410a8ab5311033e4b68085babac3e5671ca39
Abdullah Alger <abdullah.alger@ibm.com>
2016-10-27 11:29:47 -0700

    added java

---
+++ mysql-connect.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       System.setProperty("javax.net.ssl.trustStore","/home/project/truststore");
+   		 System.setProperty("javax.net.ssl.trustStorePassword","somepass");
+
+       String user = "admin";
+       String password = "mypass";
+       String URL = "jdbc:mysql://aws-us-east-1-portal.23.dblayer.com:15942/compose" +
+
+                "?verifyServerCertificate=true"+
+                "&useSSL=true" +
+                "&requireSSL=true";
+        try {
+
+            Connection conn = DriverManager.getConnection(URL, user, password);
+
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SHOW DATABASES");
+
+            while (rs.next()) {
+                String dbname = rs.getString("Database");
+                System.out.format("%s\n", dbname);
+            }
+
+            st.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Failed");
+        }
+    }
+}
